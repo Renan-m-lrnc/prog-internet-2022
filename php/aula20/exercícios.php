f276a9f601fa6d3291cb423f17f5fd2e810b0c4c
Renan Lourenção <renanmatheuslourencao@gmail.com>
2022-10-01 13:00:48 -0300

    aula 20

---
+++ php/aula20/exercícios.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Exercícios</title>
+</head>
+<body>
+<!--Exercício 1-->
+ 
+  <fieldset>
+    <caption>Exercício 1</caption>
+      <p>Hello World, meu nome é <?php "Renan" ?></p>
+  </fieldset>
+
+<!--Exercício 2-->
+  
+  <fieldset>
+    <caption>Exercício 2</caption>
+      <p>O produto de 28 e 43 é <?php 28*43?></p>
+  </fieldset>
+
+<!--Exerxício 3-->
+ 
+  <fieldset>
+    <caption>Exercício 3</caption>
+      <p>A média aritimética entre 7, 8 e 9 é: <?php (7+8+9)/3?></p>
+  </fieldset>
+
+<!--Exercício 4-->
+
+  <fieldset>
+    <caption>Exercício 4</caption>
+      <p>O valor escolhido é <?php $valor=27 ?></p>
+      <p>O sucessor de 27 é <?php $valor+1 ?></p>
+      <p>O antecessor de 27 é <?php $valor-1 ?></p>
+  </fieldset>
+
+<!--Exercício 5-->
+
+  <fieldset>
+    <caption>Exercício 5</caption>
+      <p>Os números inteiros são 15 e 78</p>
+      <p>Sua soma é <?php 15+78?></p>
+  </fieldset>
+
+<!--Exercício 6-->
+</body>
+</html>
